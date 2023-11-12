a14a50ac47395b372b2488dc88ece5a2555eb34a
FywOo02 <zhushenzhe0531@gmail.com>
2023-11-11 20:56:23 -0500

    updated login in page design

--- app/src/main/java/com/example/b07project/MainActivity.java
+++ app/src/main/java/com/example/b07project/MainActivity.java
@@ -11,7 +11,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_login_page);
         FirebaseApp.initializeApp(this);
     }
 }
