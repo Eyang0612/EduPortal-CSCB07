846ba4594d56657c73efdee00154a3e738b2889b
laurian_li <laurian.li@mail.utoronto.ca>
2023-11-11 20:29:11 -0500

    Updated Question Answer

---
+++ app/src/main/java/QuestionAnswer/FirQA.java
@@ -0,0 +1,8 @@
+package QuestionAnswer;
+
+public class FirQA {
+    public static String FirQue= "Which type of POSt are you looking for?";
+
+    public static String Choices[] ={"Minor in Computer Science", "Major or Specialist in Computer Science"};
+
+}
---
+++ app/src/main/java/QuestionAnswer/MajorQA.java
@@ -0,0 +1,14 @@
+package QuestionAnswer;
+
+public class MajorQA {
+    public static String MajorQue[]= {
+            "Please select the best answer that describes you.",
+            "Please enter your GPA (grade point average) of the following courses.  Example: MATA31: 4.0",
+    };
+
+    public static String MajorAns[][] ={
+            {"I am already in the Computer Science admission category.", "I am in another admission categories"},
+            {"CSCA08", "CSCA48", "CSCA67/MATA67", "CSCA22", "MATA31"}
+    };
+
+}
