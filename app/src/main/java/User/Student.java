8dd077f69b697335ef13eaab6be0fc6ca7230eae
Eyang0612 <eddieyang@MacBook-Air-2.local>
2023-11-12 18:39:35 -0500

    setup User and Student Object

---
+++ app/src/main/java/User/Student.java
@@ -0,0 +1,7 @@
+package User;
+
+public class Student extends User{
+    public Student(String name,String email, String password, boolean isAdmin){
+        super(name,email,password,isAdmin);
+    }
+}
---
+++ app/src/main/java/User/User.java
@@ -0,0 +1,28 @@
+package User;
+
+public abstract class User {
+    private String name;
+    private String email;
+    private String password;
+    private boolean isAdmin;
+
+    protected User(String name,String email, String password, boolean isAdmin){
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.isAdmin = isAdmin;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public boolean checkAdmin(){
+        return isAdmin;
+    }
+
+}
