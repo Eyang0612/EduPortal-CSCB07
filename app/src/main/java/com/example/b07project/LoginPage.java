c1b81567dfe6451fe508b59517dba8ee34a978bf
Owen Sun <owen.sun@mail.utoronto.ca>
2023-11-12 20:22:55 -0500

    added firebase code

--- app/src/main/java/com/example/b07project/LoginPage.java
+++ app/src/main/java/com/example/b07project/LoginPage.java
@@ -2,6 +2,7 @@ package com.example.b07project;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,6 +10,22 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import java.util.Objects;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -48,14 +65,11 @@ public class LoginPage extends AppCompatActivity {
         String password = editTextPassword.getText().toString();
         // Add your authentication logic here
         // For example, you can check the credentials against a database
-        if (isValid(password)) {
-            // Password is valid, proceed to home page
-            Intent homeIntent = new Intent(this, HomePage.class);
-            startActivity(homeIntent);
-        } else {
-            // Display an error message or handle invalid password
-            showToast("Error: Wrong email or password, please try again.");
-        }
+        isValid(email, password);
+        //else {
+        //    // Display an error message or handle invalid password
+        //    showToast("Error: Wrong email or password, please try again.");
+        //}
     }
 
     private void onSignUpClick() {
@@ -63,10 +77,50 @@ public class LoginPage extends AppCompatActivity {
         Intent signUpIntent = new Intent(this, SignUpPage.class);
         startActivity(signUpIntent);
     }
-    private boolean isValid(String password) {
+    private void isValid(String email, String password) {
         // Add your password validation logic here
         // check if email and correspond password is in database
-        return true;
+        if (TextUtils.isEmpty(email)){
+            Toast.makeText(LoginPage.this, "Email Cannot Be Empty!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)){
+            Toast.makeText(LoginPage.this, "Password Cannot Be Empty!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        DatabaseReference ref=FirebaseDatabase.getInstance().getReference("studentUsers");
+        Query checkUserDB = ref.orderByChild("email").equalTo(email);
+        checkUserDB.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    editTextEmail.setError(null);
+                    String passwordFromDB = snapshot.child(email).child("password").getValue(String.class);
+                    if (passwordFromDB.equals(password)){
+                        editTextEmail.setError(null);
+                        Intent homeIntent = new Intent(LoginPage.this, HomePage.class);
+                        startActivity(homeIntent);
+                    }else{
+                        editTextPassword.setError("Invalid Password!");
+                        editTextPassword.requestFocus();
+                        Toast.makeText(LoginPage.this, "Invalid Password!", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                }else{
+                    editTextEmail.setError("Username Does Not Exist!");
+                    editTextPassword.requestFocus();
+                    Toast.makeText(LoginPage.this, "Username Does Not Exist!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        Toast.makeText(LoginPage.this, "Test", Toast.LENGTH_SHORT).show();
+        return;
     }
     private void showToast(String message) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
