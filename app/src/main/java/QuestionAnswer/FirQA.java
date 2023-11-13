2e9b9d425a80cddeb9c3cd789772c48b77efd9d9
laurian_li <laurian.li@mail.utoronto.ca>
2023-11-12 21:32:08 -0500

    recover files

--- app/src/main/java/QuestionAnswer/FirQA.java
+++
@@ -1,8 +0,0 @@
-package QuestionAnswer;
-
-public class FirQA {
-    public static String FirQue= "Which type of POSt are you looking for?";
-
-    public static String Choices[] ={"Minor in Computer Science", "Major or Specialist in Computer Science"};
-
-}
