ac6edf836889987f444e4cea4dc3ac9bb9ddbdc3
FywOo02 <zhushenzhe0531@gmail.com>
2023-11-27 18:38:07 -0500

    created MainEventsActivity.java and SingleEventActivity.java

---
+++ app/src/main/java/com/example/b07project/studentPages/EventCheck/SingleEventActivity.java
@@ -0,0 +1,16 @@
+package com.example.b07project.studentPages.EventCheck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.b07project.R;
+
+public class SingleEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_event);
+    }
+}
