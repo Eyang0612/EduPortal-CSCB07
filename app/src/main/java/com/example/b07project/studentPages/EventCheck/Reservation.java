b4b48b6c1b14ad8a5ed973b279debf06d91a66c5
FywOo02 <zhushenzhe0531@gmail.com>
2023-11-28 11:43:00 -0500

    updated the logic code of SingleEventActivity.java

--- app/src/main/java/com/example/b07project/studentPages/EventCheck/Reservation.java
+++ app/src/main/java/com/example/b07project/studentPages/EventCheck/Reservation.java
@@ -1,6 +1,8 @@
 package com.example.b07project.studentPages.EventCheck;
 
-public class Reservation {
+import java.io.Serializable;
+
+public class Reservation implements Serializable {
     private String eventId;
     private String studentId;
 
